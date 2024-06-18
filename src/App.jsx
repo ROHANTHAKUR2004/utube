d201874b07072ec0990d606c2a07e869faac60d8
rohan <rohanthakur89768@gmail.com>
2024-06-18 10:42:14 +0530

    added navbar and react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Pages/Home/Home'
+import Video from './Pages/Video/Video'
 
 function App() {
   
 
   return (
     <>
-     hello yt
+     <Navbar/>
+     <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/video/:categoryId/:videoId' element={<Video/>} />
+     </Routes>
     </>
   )
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Navbar.css'
+import menu from '../../assets/menu/menu.png'
+import logo from  '../../assets/logo/logo.png'
+import search from '../../assets/search/search.png'
+import upload from '../../assets/upload/upload.png'
+import icon from '../../assets/more/more.png'
+import noti from '../../assets/notification/notification.png'
+import profile from '../../assets/jack/jack.png'
+
+const Navbar = () => {
+  return (
+    <nav className='flex-div'>
+        <div className='nav-left flex-div'>
+            <img className='menu-icon' src={menu} alt="" />
+            <img className='logo' src={logo} alt="" />
+        </div>
+      
+      <div className="nav-middle  flex-div ">
+        <div className="search-box flex-div ">
+        <input type="text" placeholder='Search.'/>
+        <img src={search} alt="" />
+        </div>  
+      </div>
+
+      <div className="nav-right flex-div">
+        <img src={upload}    alt="" />
+        <img src={icon}      alt="" />
+        <img src={noti}      alt="" />
+        <img src={profile} className='user-icon'  alt="" />
+      </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
