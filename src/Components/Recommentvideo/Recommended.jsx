8ac1e594da8d09b42d08d2575e47dd27bc02af92
rohan <rohanthakur89768@gmail.com>
2024-06-19 10:31:19 +0530

    added recommended commponent

--- src/Components/Recommentvideo/Recommended.jsx
+++ src/Components/Recommentvideo/Recommended.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './Recommended.css'
+import thumb1 from '../../assets/thumbnail/thumbnail1.png'
+import thumb2 from '../../assets/thumbnail/thumbnail2.png'
+import thumb3 from '../../assets/thumbnail/thumbnail3.png'
+import thumb4 from '../../assets/thumbnail/thumbnail4.png'
+
 const Recommended = () => {
   return (
-    <div>
-      Recommended vdieos
+    <div className='recomended'>
+        <div className="side-vid-list">
+            <img src={thumb1} alt="" />
+            <div className="vid-info">
+                <h4>Lorem, ipsum dolor.</h4>
+                <p>RohanYT</p>
+                <p>2k Views</p>
+            </div>
+        </div>
+
+        <div className="side-vid-list">
+            <img src={thumb2} alt="" />
+            <div className="vid-info">
+                <h4>Lorem, ipsum dolor.</h4>
+                <p>RohanYT</p>
+                <p>2k Views</p>
+            </div>
+        </div>
+
+        <div className="side-vid-list">
+            <img src={thumb3} alt="" />
+            <div className="vid-info">
+                <h4>Lorem, ipsum dolor.</h4>
+                <p>RohanYT</p>
+                <p>2k Views</p>
+            </div>
+        </div>
+
+
+        <div className="side-vid-list">
+            <img src={thumb4} alt="" />
+            <div className="vid-info">
+                <h4>Lorem, ipsum dolor.</h4>
+                <p>RohanYT</p>
+                <p>2k Views</p>
+            </div>
+        </div>
+
     </div>
   )
 }
